053c632e6ec95540ecf7a55ccda2ab372dce796b
Monica Guzman <a00838824@tec.mx>
2025-10-08 12:35:10 -0600

    Cambie fonts :D

--- src/views/MaesActivos.vue
+++ src/views/MaesActivos.vue
@@ -117,11 +117,13 @@ const filterSubjects = () => {
             </h1>
         </span>
         
-        <h1 class="text-black text-4xl font-bold text-left md:text-center md:text-left mt-3" v-else> 
+        <h1 class="text-black text-6xl font-bold text-center m-0 sm:text-left" v-else> 
              MAEs activos
         </h1>
     </div>
-
+    
+    <!-- Buscar entre los maes activos -->
+    <h2 class="text-black text-3xl font-semibold text-center sm:text-left">Filtros</h2>
     <div class="flex md:flex-row flex-column mb-4">
             <span class="w-full md:w-5 mt-3 mr-3">
                 <InputText v-model="nombreInput" placeholder="Nombre..." class="w-full" />
